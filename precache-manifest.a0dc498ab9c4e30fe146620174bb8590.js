ca1bb5d596b01333d11826f0bdf8c146e3fcc612
Hanna Bun <hannabun@Hannas-MacBook-Air.local>
2020-09-16 20:45:57 +0800

    Updates

---
+++ precache-manifest.a0dc498ab9c4e30fe146620174bb8590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5afe4557044f0db15a76abe5cb389f",
+    "url": "/HannaB-dev.github.io/index.html"
+  },
+  {
+    "revision": "3eeecb244c3e579a4415",
+    "url": "/HannaB-dev.github.io/static/css/main.02ce36a1.chunk.css"
+  },
+  {
+    "revision": "f1b8a923bcafeed85fd8",
+    "url": "/HannaB-dev.github.io/static/js/2.6f86c3e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HannaB-dev.github.io/static/js/2.6f86c3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eeecb244c3e579a4415",
+    "url": "/HannaB-dev.github.io/static/js/main.345892ef.chunk.js"
+  },
+  {
+    "revision": "7b51bcaa2146c2b6a126",
+    "url": "/HannaB-dev.github.io/static/js/runtime-main.c2d2908d.js"
+  }
+]);
